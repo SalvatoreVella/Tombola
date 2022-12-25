7b1ee6759aa5556e7cf678ae84d376b558d00ab2
Salvatore Vella <113929059+SalvatoreVella@users.noreply.github.com>
2022-12-25 20:01:56 +0100

    Update clean

--- tombola.js
+++ tombola.js
@@ -1,11 +1,6 @@
 const $allNumbersContainer = document.querySelector(".numbers-container");
 const $button = document.querySelector("button");
-const $bestemmia = document.querySelector(".bestemmia");
 const $bigNumContainer = document.querySelector(".current-number");
-
-let bestemmie = ["Dio Porco", "Dio Gallina", "Diu Smarmittatu", "Porco il Cristo", "Buttana La Madonna", 
-"Ma che oooh!", "Se non bestemmio, guarda!", "Cristu Crastu", "Madonna Avvelenata", "Diu Bastardu", "Dio becco", "porco Gesù",
-"Bastardo san Giuseppe", "Dio Maiale", "Madonna Sverginata", "Gesu crasto", "Agnello di Dio, Becco di Dio"]
 
 let numbers = [];
 
@@ -21,10 +16,6 @@ numbersShuffle.sort(() => {
 
 let counter = 0;
 
-function seeNumber() {
-    numbersShuffle[counter]
-}
-
 let html = numbers.map((number) => {
    return `<div class="number">${number}</div>`
 }).join("");
@@ -34,7 +25,6 @@ $allNumbersContainer.innerHTML = html;
 $button.addEventListener("click", () => {
     if (numbersShuffle[counter]) {
         $bigNumContainer.innerHTML = numbersShuffle[counter];
-        $bestemmia.innerHTML = bestemmie[Math.ceil(Math.random() * (bestemmie.length - 1))];
         let thisNumbers = document.querySelectorAll(".number");
         thisNumbers.forEach((number) => {
             if (number.innerHTML == numbersShuffle[counter]) {
